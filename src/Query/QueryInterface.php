d0d2337d098d36166b381c7311281ace1a9a3887
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-13 09:43:22 +0100

    Removed query features from `QueryInterface`

--- src/Query/QueryInterface.php
+++ src/Query/QueryInterface.php
@@ -5,19 +5,8 @@ namespace Dhii\Storage\Query;
 /**
  * Represents the specifics of the query.
  *
- * Specifically:
- * - Any conditions of the query;
- * - Conditions that directly determine what data to return.
- * - Ordering, limiting, and grouping instructions.
- * - Post-retrieval filtering.
- * - Instructions for involving data from other sources.
- *
  * @since [*next-version*]
  */
 interface QueryInterface extends
-    ConditionAwareInterface,
-    FieldsAwareInterface,
-    GroupingAwareInterface,
-    LimitAwareInterface
 {
 }
